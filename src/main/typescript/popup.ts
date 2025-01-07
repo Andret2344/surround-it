2699376e0eaae3994288de8076ace36228548b05
Andret2344 <andret2344@gmail.com>
2025-01-07 08:55:07 +0100

    Small UX fixes
    Took 32 minutes

--- src/main/typescript/popup.ts
+++ src/main/typescript/popup.ts
@@ -1,5 +1,6 @@
 import '../scss/popup.scss';
 import {runWithActive, setActive} from './service/StorageService';
+import browser from 'webextension-polyfill';
 
 const powerButton: Element | null = document.querySelector('#power-button');
 if (powerButton) {
@@ -10,3 +11,7 @@ if (powerButton) {
 			(): void => setActive(false, (): void => powerButton.classList.remove('active')),
 			(): void => setActive(true, (): void => powerButton.classList.add('active'))));
 }
+
+document.querySelector('#options-button')?.addEventListener('click', (): void => {
+	browser.runtime.openOptionsPage()
+});
